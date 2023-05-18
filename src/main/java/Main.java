03c5de7c385103ac881f7b7f229a7364ccf9b8f5
q20142015 <q20142015@yandex.ru>
2023-05-18 11:53:22 +0300

    шифт л

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,34 +2,36 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-    public class Main {
+public class Main {
 
-        public static void main(String[] args) {
-            String srcFolder = "C:/Users/Slava/Desktop/src";
-            String dstFolder = "C:/Users/Slava/Desktop/dst";
+    public static void main(String[] args) {
+        String srcFolder = "C:/Users/Slava/Desktop/src";
+        String dstFolder = "C:/Users/Slava/Desktop/dst";
 
-            File srcDir = new File(srcFolder);
+        File srcDir = new File(srcFolder);
 
-            long start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 
-            File[] files = srcDir.listFiles();
+        File[] files = srcDir.listFiles();
 
-           int nimP= Runtime.getRuntime().availableProcessors();
+        int nimP = Runtime.getRuntime().availableProcessors();
 
-            int midle=files.length/nimP;
-            int nach=0;
-            int con=nach+midle;
-           for (int i=0;i<nimP;i++){
-           File[] file1=new File[con-nach];
-           System.arraycopy(files,nach,file1,0,con-nach);
-ImageResizer image1=new ImageResizer(file1,dstFolder,start);
-new Thread(image1).start();
-nach=con;
-con=(i+2)*midle;
-if(i==nimP-2){con=files.length;}
-           }
-
+        int midle = files.length / nimP;
+        int nach = 0;
+        int con = nach + midle;
+        for (int i = 0; i < nimP; i++) {
+            File[] file1 = new File[con - nach];
+            System.arraycopy(files, nach, file1, 0, con - nach);
+            ImageResizer image1 = new ImageResizer(file1, dstFolder, start);
+            new Thread(image1).start();
+            nach = con;
+            con = (i + 2) * midle;
+            if (i == nimP - 2) {
+                con = files.length;
+            }
         }
+
     }
+}
 
 
